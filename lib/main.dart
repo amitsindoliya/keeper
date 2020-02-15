80faa00a58ee945774cbb41bdb4890f96d52fe8a
Amit <arsindoliya@gmail.com>
2020-02-15 20:21:22 +0530

    basic keeper layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import './models/Entries.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    final List<Entries> entriesList = [
+      Entries(11, "Amit", "wallet", DateTime.now()),
+      Entries(12, "Cakey", "cakes", DateTime.now())
+    ];
+
     return MaterialApp(
       title: "keeper",
       home: Scaffold(
@@ -14,11 +19,63 @@ class MyApp extends StatelessWidget {
           backgroundColor: Colors.red,
           elevation: 0.0,
         ),
-        body: Container(
+        body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              Text("Line 1"),
-              Text("Line 2")
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Token Number",
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: "Name",
+                ),
+              ),
+              Container(
+                height: 500,
+                padding: EdgeInsets.all(10),
+                child: ListView.builder(
+                  itemBuilder: (ctx, index) {
+                    return Card(
+                      elevation: 1.0,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          
+                          Container(
+                            height:60,
+                            child: CircleAvatar(
+                              radius: 30,
+                              child: Text(
+                                "${entriesList[index].token}",
+                              ),
+                            ),
+                          ),
+                          Column(
+                            children: <Widget>[
+                              Text(
+                                "${entriesList[index].name}",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                "${entriesList[index].items}",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                  itemCount: entriesList.length,
+                ),
+              )
             ],
           ),
         ),
---
+++ lib/models/Entries.dart
@@ -0,0 +1,8 @@
+class Entries{
+  final int token;
+  final String name;
+  final DateTime date;
+  final String items;
+
+  Entries(this.token,this.name,this.items,this.date);
+}
